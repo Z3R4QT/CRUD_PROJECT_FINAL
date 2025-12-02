9043ff86ff959b08f36d7ff28dd3f24cf8138001
Z3R4QT <christopher092480@gmail.com>
2025-12-02 19:49:44 +0800

    update

--- CRUD_PROJECT_FINAL/UC_AddEmployee.vb
+++ CRUD_PROJECT_FINAL/UC_AddEmployee.vb
@@ -2,22 +2,21 @@
 
 Public Class UC_AddEmployee
 
-        Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
 
         Dim con As New MySqlConnection("server=localhost;user=root;password=;database=yourdb")
         Dim cmd As New MySqlCommand("INSERT INTO employees (name, position, salary, department)
                                      VALUES (@name, @pos, @salary, @dept)", con)
 
         cmd.Parameters.AddWithValue("@name", txtName.Text)
-            cmd.Parameters.AddWithValue("@pos", txtPosition.Text)
-            cmd.Parameters.AddWithValue("@salary", txtSalary.Text)
-            cmd.Parameters.AddWithValue("@dept", txtDepartment.Text)
+        cmd.Parameters.AddWithValue("@pos", txtPosition.Text)
+        cmd.Parameters.AddWithValue("@salary", txtSalary.Text)
+        cmd.Parameters.AddWithValue("@dept", txtDepartment.Text)
 
-            con.Open()
-            cmd.ExecuteNonQuery()
+        con.Open()
+        cmd.ExecuteNonQuery()
         con.Close()
 
         MessageBox.Show("Employee added successfully!")
-        End Sub
-
-    End Class
+    End Sub
+End Class
